75a260bc56ffe56778790c961d05e855476b1b94
Josh O'Connell <ateagletek@gmail.com>
2017-10-24 21:23:47 -0400

    Add Procfile, seed database, specify ruby version

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+City.destroy_all
+State.destroy_all
+
+City.create(name: "Baltimore")
+State.create(name: "Maryland")
