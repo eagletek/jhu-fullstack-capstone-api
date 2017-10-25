80e35e31df168efcf82ab2c8e02a0f9b06df9b8d
Josh O'Connell <ateagletek@gmail.com>
2017-10-24 20:59:23 -0400

    Add City and State resources

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
   get '/ui' => 'ui#index'
   get '/ui#' => 'ui#index'
 
+  scope :api, defaults: {format: :json} do
+    resources :cities, except: [:new, :edit]
+    resources :states, except: [:new, :edit]
+  end
 end
