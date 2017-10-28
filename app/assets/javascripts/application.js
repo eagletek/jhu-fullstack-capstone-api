57bdb8c9830e7cfef0af0b5420c636fdeb20340b
Josh O'Connell <ateagletek@gmail.com>
2017-10-27 22:18:00 -0400

    Create angular app shell with home state

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,3 +4,7 @@
 //= require angular
 //= require angular-ui-router
 //= require angular-resource
+//
+//= require photo-tourist/app.module
+//= require photo-tourist/app.router
+//= require photo-tourist/app.constant
---
+++ app/assets/javascripts/photo-tourist/app.router.js
@@ -0,0 +1,24 @@
+(function() {
+  "use strict";
+
+  angular
+    .module("photo-tourist")
+    .config(RouterFunction);
+
+  RouterFunction.$inject = ["$stateProvider",
+                            "$urlRouterProvider",
+                            "photo-tourist.APP_CONFIG"];
+
+  function RouterFunction($stateProvider, $urlRouterProvider, APP_CONFIG) {
+    $stateProvider
+      .state("home", {
+        url: "/",
+        templateUrl: APP_CONFIG.main_page_html,
+        // controller: ,
+        // controllerAs: ,
+      })
+
+      $urlRouterProvider.otherwise("/");
+  };
+
+}());
