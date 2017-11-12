64a2dc044b134f2ab2fdf84a1133de4ba5428e3c
Josh O'Connell <ateagletek@gmail.com>
2017-11-12 17:21:35 -0500

    API: signup errors

--- spec/requests/auth_apis_spec.rb
+++ spec/requests/auth_apis_spec.rb
@@ -7,9 +7,7 @@ RSpec.describe "Authentication Api", type: :request do
   context 'signup' do
     context 'with valid registration' do
       it "successfully creates account" do
-        jpost user_registration_path, user_props
-        #pp parsed_body
-        expect(response).to have_http_status(:ok)
+        signup user_props
 
         payload=parsed_body
         expect(payload).to include("status"=>"success")
@@ -25,11 +23,32 @@ RSpec.describe "Authentication Api", type: :request do
 
     context 'with invalid registration' do
       context 'missing content' do
-        it "reports error with messages"
+        it "reports error with messages" do
+          signup user_props.except(:email), :unprocessable_entity
+
+          payload = parsed_body
+          expect(payload).to include("status"=>"error")
+          expect(payload).to include("data")
+          expect(payload['data']).to include("email"=>nil)
+          expect(payload['errors']).to include("email")
+          expect(payload['errors']).to include("full_messages")
+          expect(payload['errors']['full_messages']).to include(/Email/i)
+        end
       end
 
       context 'non-unique information' do
-        it "reports non-unique email"
+        it "reports non-unique email" do
+          signup user_props
+          signup user_props, :unprocessable_entity
+
+          payload = parsed_body
+          expect(payload).to include("status"=>"error")
+          expect(payload).to include("data")
+          expect(payload['data']).to include("created_at"=>nil)
+          expect(payload['errors']).to include("email")
+          expect(payload['errors']).to include("full_messages")
+          expect(payload['errors']['full_messages']).to include(/taken/i)
+        end
       end
     end
   end
