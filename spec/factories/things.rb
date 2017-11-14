df7d0804511ef6e39bd73510b752264a016afc21
Josh O'Connell <ateagletek@gmail.com>
2017-11-13 22:32:55 -0500

    Server: ThingImage model

--- spec/factories/things.rb
+++ spec/factories/things.rb
@@ -25,5 +25,14 @@ FactoryGirl.define do
     trait :with_notes do
       notes { Faker::HarryPotter.quote }
     end
+
+    trait :with_image do
+      transient do
+        image_count 1
+      end
+      after(:build) do |thing, props|
+        thing.thing_images << build_list(:thing_image, props.image_count, :thing=>thing)
+      end
+    end
   end
 end
