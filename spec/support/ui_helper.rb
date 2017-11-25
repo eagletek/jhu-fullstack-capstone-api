10cad65dbb89d64740d4736fc17b73153151f317
Josh O'Connell <ateagletek@gmail.com>
2017-11-24 22:24:30 -0500

    Server: ThingImage Role Authorization

--- spec/support/ui_helper.rb
+++ spec/support/ui_helper.rb
@@ -1,28 +1,27 @@
 module UiHelper
   def create_user
-    user_props = FactoryGirl.attributes_for(:user);
+    user_props=FactoryGirl.attributes_for(:user);
     user = FactoryGirl.create(:user, user_props)
     user_props.merge(:id=>user.id, :uid=>user.uid)
   end
 
   def fillin_signup registration
-    visit("#{ui_path}/#/signup") unless page.has_css?("#signup-form")
-    using_wait_time 5 do
-      expect(page).to have_css("#signup-form")
-    end
+    visit "#{ui_path}/#/signup" unless page.has_css?("#signup-form")
+    expect(page).to have_css("#signup-form",:wait=>5)
 
-    fill_in("signup-email", with: registration[:email])
-    fill_in("signup-name", with: registration[:name])
-    fill_in("signup-password", with: registration[:password])
+    fill_in("signup-email", :with=>registration[:email])
+    fill_in("signup-name", :with=>registration[:name])
+    fill_in("signup-password", :with=>registration[:password])
     password_confirm = registration[:password_confirmation] ||= registration[:password]
-    fill_in("signup-password_confirmation", with: password_confirm)
+    fill_in("signup-password_confirmation", :with=>password_confirm)
   end
 
   def signup registration, success=true
     fillin_signup registration
-    click_on("Sign Up")
+    expect(page).to have_button("Sign Up",:disabled=>false) if success
+    click_on("Sign Up")  
     if success
-      expect(page).to have_no_button("Sign Up")
+      expect(page).to have_no_button("Sign Up", :wait=>5)
     else
       expect(page).to have_button("Sign Up")
     end
@@ -31,15 +30,15 @@ module UiHelper
   def logged_in? account=nil
     account ?
       page.has_css?("#navbar-loginlabel",:text=>/#{account[:name]}/) :
-      page.has_css?("#user-id",:visible=>false)
+      page.has_css?("#user_id",:text=>/.+/, :visible=>false)
   end
 
   def fillin_login credentials
     visit root_path unless page.has_css?("#navbar-loginlabel")
-    find("#navbar-loginlabel", text: "Login").click
+    find("#navbar-loginlabel",:text=>"Login").click
     within("#login-form") do
-      fill_in("login_email", with: credentials[:email])
-      fill_in("login_password", with: credentials[:password])
+      fill_in("login_email", :with=>credentials[:email])
+      fill_in("login_password", :with=>credentials[:password])
     end
   end
 
@@ -52,16 +51,23 @@ module UiHelper
     using_wait_time 5 do
       expect(page).to have_no_css("#login-form")
     end
-    expect(page).to have_css("#navbar-loginlabel", text:/#{credentials[:name]}/)
-    expect(page).to have_css("#logout-form", visible: false)
+    expect(page).to have_css("#logout-form", :visible=>false)
+    expect(page).to have_css("#navbar-loginlabel",:text=>/#{credentials[:name]}/)
+    return credentials
   end
 
   def logout
     if logged_in?
-      find("#navbar-loginlabel").click
-      find_button("Logout").click
-      expect(page).to have_no_css("#user-id",:visible=>false)
+      find("#navbar-loginlabel").click unless page.has_button?("Logout")
+      find_button("Logout",:wait=>5).click
+      expect(page).to have_no_css("#user_id",:visible=>false,:wait=>5)
     end
   end
 
+  def wait_until
+    Timeout.timeout(Capybara.default_max_wait_time) do 
+      sleep(0.1) until value = yield
+      value
+    end
+  end
 end
